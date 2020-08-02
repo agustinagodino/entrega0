54f42d2f814cce6bffcc8ce425d1a1b3c4ba3842
agustinagodino <68866036+agustinagodino@users.noreply.github.com>
2020-08-02 17:26:18 -0300

    entrega0

---
+++ evento.js
@@ -0,0 +1,8 @@
+function mostrarDescripcion(idDescripcion) {
+
+    // EXTRAEMOS EL ELEMENTO OCULTO
+    var parrafo = document.getElementById(idDescripcion);
+
+    // MOSTRAMOS EL ELEMENTO
+    parrafo.style.display = "block";
+}
